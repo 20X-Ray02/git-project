5000547caba0cd59d397ceb4cdac04bab3f26839
20X-Ray02 <89675111+20X-Ray02@users.noreply.github.com>
2023-09-11 01:33:00 +0700

    dorokhov second commit

--- main.py
+++ main.py
@@ -24,7 +24,22 @@ class Example(Frame):
             canvas.create_polygon(835 / 10 * scl + x, 135 / 10 * scl + y, 835 / 10 * scl + x, 125 / 10 * scl + y, 1140 / 10 * scl + x, 185 / 10 * scl + y, 1140 / 10 * scl + x, 195 / 10 * scl + y, fill='#d2691e', outline='black'),
             canvas.create_polygon(150 / 10 * scl + x, 185 / 10 * scl + y, 835 / 10 * scl + x, 125 / 10 * scl + y, 675 / 10 * scl + x, 50 / 10 * scl + y, fill='#b22222', outline='black'),
             canvas.create_polygon(835 / 10 * scl + x, 125 / 10 * scl + y, 675 / 10 * scl + x, 50 / 10 * scl + y, 1140 / 10 * scl + x, 185 / 10 * scl + y, fill='#b22222', outline='black'),
-            
+            #Стены
+            canvas.create_polygon(200 / 10 * scl + x, 200 / 10 * scl + y, 200 / 10 * scl + x, 500 / 10 * scl + y, 800 / 10 * scl + x, 600 / 10 * scl + y, 800 / 10 * scl + x, 150 / 10 * scl + y, fill='#e0e094', outline='black'),
+            canvas.create_polygon(800 / 10 * scl + x, 150 / 10 * scl + y, 800 / 10 * scl + x, 600 / 10 * scl + y, 1100 / 10 * scl + x, 475 / 10 * scl + y, 1100 / 10 * scl + x, 200 / 10 * scl + y, fill='#e0e094', outline='black'),
+            #Дополнительные элементы
+            canvas.create_polygon(200 / 10 * scl + x, 200 / 10 * scl + y, 200 / 10 * scl + x, 210 / 10 * scl + y, 800 / 10 * scl + x, 160 / 10 * scl + y, 800 / 10 * scl + x, 150 / 10 * scl + y, fill='#d2691e', outline='black'),
+            canvas.create_polygon(800 / 10 * scl + x, 150 / 10 * scl + y, 800 / 10 * scl + x, 160 / 10 * scl + y, 1100 / 10 * scl + x, 206 / 10 * scl + y, 1100 / 10 * scl + x, 200 / 10 * scl + y, fill='#d2691e', outline='black'),
+            canvas.create_polygon(200 / 10 * scl + x, 320 / 10 * scl + y, 200 / 10 * scl + x, 350 / 10 * scl + y, 800 / 10 * scl + x, 375 / 10 * scl + y, 800 / 10 * scl + x, 345 / 10 * scl + y, fill='#d2691e', outline='black'),
+            canvas.create_polygon(800 / 10 * scl + x, 345 / 10 * scl + y, 800 / 10 * scl + x, 374 / 10 * scl + y, 1100 / 10 * scl + x, 340 / 10 * scl + y, 1100 / 10 * scl + x, 322 / 10 * scl + y, fill='#d2691e', outline='black'),
+            canvas.create_polygon(200 / 10 * scl + x, 480 / 10 * scl + y, 200 / 10 * scl + x, 500 / 10 * scl + y, 800 / 10 * scl + x, 600 / 10 * scl + y, 800 / 10 * scl + x, 580 / 10 * scl + y,fill='#c1bba4', outline='black'),
+            canvas.create_polygon(200 / 10 * scl + x, 490 / 10 * scl + y, 200 / 10 * scl + x, 500 / 10 * scl + y, 800 / 10 * scl + x, 600 / 10 * scl + y, 800 / 10 * scl + x, 590 / 10 * scl + y, fill='#c1bba4', outline='black'),
+            canvas.create_polygon(800 / 10 * scl + x, 580 / 10 * scl + y, 800 / 10 * scl + x, 600 / 10 * scl + y, 1100 / 10 * scl + x, 475 / 10 * scl + y, 1100 / 10 * scl + x, 460 / 10 * scl + y,fill='#c1bba4', outline='black'),
+            canvas.create_polygon(800 / 10 * scl + x, 590 / 10 * scl + y, 800 / 10 * scl + x, 600 / 10 * scl + y, 1100 / 10 * scl + x, 475 / 10 * scl + y, 1100 / 10 * scl + x, 468 / 10 * scl + y, fill='#c1bba4', outline='black'),
+            #Дверь
+            canvas.create_polygon(295 / 10 * scl + x, 355 / 10 * scl + y, 295 / 10 * scl + x, 496 / 10 * scl + y, 350 / 10 * scl + x, 505 / 10 * scl + y, 350 / 10 * scl + x, 357 / 10 * scl + y, fill='#d2691e', outline='black'),
+            canvas.create_polygon(300 / 10 * scl + x, 355 / 10 * scl + y, 300 / 10 * scl + x, 495 / 10 * scl + y, 350 / 10 * scl + x, 505 / 10 * scl + y, 350 / 10 * scl + x, 357 / 10 * scl + y, fill='#1d3ec2', outline='black', width='3'),
+
         canvas.pack(fill=BOTH, expand=True)
 
 
